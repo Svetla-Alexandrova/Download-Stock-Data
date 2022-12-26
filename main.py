cc18d45251396233f01d8561049044d1eb3abd83
Svetla Alexandrova <121426643+Svetla-Alexandrova@users.noreply.github.com>
2022-12-26 15:43:44 +0000

    Select company ticker, start date and end date

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import requests
+from datetime import datetime
+import time
 
-url = "https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=345427200&period2=1672012800&interval=1d&events=history&includeAdjustedClose=true"
+ticker = input("Enter the ticker symbol: ")
+from_date = input('Enter start date in yyyy/mm/dd format:')
+to_date = input('Enter end date in yyyy/mm/dd format:')
+
+from_datetime = datetime.strptime(from_date, '%Y/%m/%d')
+to_datetime = datetime.strptime(to_date, '%Y/%m/%d')
+
+from_epoch = int(time.mktime(from_datetime.timetuple()))
+to_epoch = int(time.mktime(to_datetime.timetuple()))
+
+
+url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={from_epoch}&period2={to_epoch}&interval=1d&events=history&includeAdjustedClose=true"
 
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
 content = requests.get(url, headers=headers).content
